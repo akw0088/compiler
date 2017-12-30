4c3e4ba584f2220a670869291adb35dbf7cb8fdf
awright <akw0088@unt.edu>
2017-12-30 15:58:41 -0800

    add output and run.sh

---
+++ run.sh
@@ -0,0 +1,20 @@
+./progs/one > ./output/one.txt
+./progs/two > ./output/two.txt
+./progs/three > ./output/three.txt
+./progs/four > ./output/four.txt
+./progs/five > ./output/five.txt
+./progs/six > ./output/six.txt
+./progs/seven > ./output/seven.txt
+./progs/eight > ./output/eight.txt
+./progs/nine > ./output/nine.txt
+./progs/ten > ./output/ten.txt
+./progs/8q > ./output/8q.txt
+./progs/bubble > ./output/bubble.txt
+./progs/hanoi > ./output/hanoi.txt
+./progs/queens > ./output/queens.txt
+./progs/mod > ./output/mod.txt
+./progs/multiDimension > ./output/multiDimension.txt
+./progs/factorial > ./output/factorial.txt
+./progs/sum > ./output/sum.txt
+./progs/scope > ./output/scope.txt
+
